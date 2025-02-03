b34cf9d906b0f32490c5343fff54e0abbbc8a4d8
a.feiz <amirfeiz7@gmail.com>
2025-02-03 08:56:18 +0330

    Spring-security : security config - no password encoder

---
+++ spring-security/src/main/java/com/amirfeiz/spring_security/service/MyUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.amirfeiz.spring_security.service;
+
+import com.amirfeiz.spring_security.model.User;
+import com.amirfeiz.spring_security.model.UserPrincipal;
+import com.amirfeiz.spring_security.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username);
+        if (user == null) {
+            System.out.println("User not found");
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new UserPrincipal(user);
+    }
+}
